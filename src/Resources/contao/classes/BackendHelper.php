d45f1d95817f135820d445f78f959a3ca25c1ed0
doishub <daniele@oveleon.de>
2021-05-28 10:29:56 +0200

    Add text highlighting

--- src/Resources/contao/classes/BackendHelper.php
+++ src/Resources/contao/classes/BackendHelper.php
@@ -18,13 +18,34 @@ class BackendHelper extends Backend
         // Execute the default function
         $strRow  = call_user_func_array(['tl_article', 'addIcon'], [$row, $label]);
 
+        if(!$this->User->article_info_style === 'none')
+        {
+            return $strRow;
+        }
+
         // Wrap content
         $strRow  = '<span>' . $strRow . '</span>';
 
         // Extend article info
         if(!!$row['article_info'])
         {
-            $strRow .= '<span class="art-info" title="' . $row['article_info'] . '">' . $row['article_info'] . '</span>';
+            // Highlight info
+            $hlInfo = html_entity_decode($row['article_info']);
+            $hlInfo = preg_replace(
+                [
+                    '/\{([^+]+)\}/iU',
+                    '/\[([^+]+)\]/iU',
+                    '/\#([^+]+)\#/iU'
+                ],
+                [
+                    '<span class="bh-class">$1</span>',
+                    '<span class="bh-tag">$1</span>',
+                    '<span class="bh-id">$1</span>'
+                ],
+                $hlInfo
+            );
+
+            $strRow .= '<span class="art-info" title="' . $row['article_info'] . '">' . $hlInfo . '</span>';
         }
 
         return '<div class="tl_row_inner ' . ($this->User->article_info_style ?: '') . '">' . $strRow . '</div>';
