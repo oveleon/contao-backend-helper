43db8ca4943b0982bb942fad7d2c03b3b2134cff
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-03-31 08:47:14 +0200

    Load plugin CSS via event subscriber

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,9 +1,1 @@
 <?php
-use Contao\System;
-
-$request = System::getContainer()->get('request_stack')->getCurrentRequest();
-
-if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
-{
-    $GLOBALS['TL_CSS'][] = 'bundles/contaobackendhelper/backend/css/backend.css';
-}
