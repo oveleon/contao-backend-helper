21dd986e02c09cc597ed7079a4648751c4673865
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-03-31 08:34:31 +0200

    Reorder CSS files

--- contao/config/config.php
+++ contao/config/config.php
@@ -5,5 +5,5 @@ $request = System::getContainer()->get('request_stack')->getCurrentRequest();
 
 if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
 {
-    $GLOBALS['TL_CSS'][] = 'bundles/contaobackendhelper/backend.css';
+    $GLOBALS['TL_CSS'][] = 'bundles/contaobackendhelper/backend/css/backend.css';
 }
