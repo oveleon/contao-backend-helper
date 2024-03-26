386fdc5559d0d25f3df3c62a406de721f6f01f4d
Sebastian Zoglowek <zumklo@googlemail.com>
2024-03-26 11:51:58 +0100

    Fixed default value for article info

--- contao/dca/tl_user.php
+++ contao/dca/tl_user.php
@@ -17,7 +17,7 @@ $GLOBALS['TL_DCA']['tl_user']['fields']['article_info_placeholder'] = [
     'inputType'               => 'checkbox',
     'options'                 => ['id', 'tag', 'class'],
     'eval'                    => ['tl_class'=>'w50', 'multiple' => true],
-    'sql'                     => 'varchar(255) NOT NULL default "a:3:{i:0;s:2:"id";i:1;s:3:"tag";i:2;s:5:"class";}"'
+    'sql'                     => "varchar(255) NOT NULL default 'a:3:{i:0;s:2:\"id\";i:1;s:3:\"tag\";i:2;s:5:\"class\";}'"
 ];
 
 // Extend the default palettes
